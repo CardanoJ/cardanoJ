23252cd50827d3a8453c07b176017d95b3f1a41c
“Silesh <sileshdash95@gmail.com>
2024-03-15 22:12:09 +0530

    Transaction Sucessfully

--- src/main/java/org/cardanoJ/Main.java
+++ src/main/java/org/cardanoJ/Main.java
@@ -1,11 +1,15 @@
 package org.cardanoJ;
 
+import org.cardanoJ.transaction.BuildTransaction;
 import org.cardanoJ.wallet.CreateWallet;
 
 public class Main {
     public static void main(String[] args) {
-        CreateWallet createWallet = new CreateWallet();
+        CreateWallet createWallet = new CreateWallet();   //Wallet Creation
         createWallet.create();
-        
+
+
+        BuildTransaction buildTransaction= new BuildTransaction();  // Build Transaction
+        buildTransaction.transact();
     }
 }
