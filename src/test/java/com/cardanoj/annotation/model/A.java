785f51ce71ac8a020bf900050e6757f7ba19b1e1
Tarachand <tarachandmahto1999@gmail.com>
2024-04-25 14:38:26 +0530

    test cases added

---
+++ src/test/java/com/cardanoj/annotation/model/A.java
@@ -0,0 +1,14 @@
+package com.cardanoj.annotation.model;
+
+import com.cardanoj.plutus.annotation.Constr;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Constr
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class A {
+    String name;
+}
