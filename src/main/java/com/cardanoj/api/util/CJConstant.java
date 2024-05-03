dab7588b3732774ee45eb613126601ecd89c392e
sumita <sumita7537@gmail.com>
2024-05-03 18:59:54 +0530

    Initial commit

---
+++ src/main/java/com/cardanoj/api/util/CJConstant.java
@@ -0,0 +1,12 @@
+package com.cardanoj.api.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CJConstant {
+	  public static final String MAINNET = "mainnet";
+	    public static final String TESTNET = "--testnet-magic";
+	    public static final String cliPath= "/usr/local/bin/cardano-cli";
+	    public static final String socketPath = "/home/tusar/git/cardano-node/preview/node.socket";
+
+}
