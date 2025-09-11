7cd9c0fe422e83db5c1572bc7ed8f7f5d930b803
Snehasish Paul <snehasish.paul@aiquantgroup.com>
2025-09-11 10:01:42 +0530

    webconfig update backend

--- dapp-1/cardanoj-backend/src/main/java/com/aiquant/cardanoJ/example/dapp1/WebConfig.java
+++ dapp-1/cardanoj-backend/src/main/java/com/aiquant/cardanoJ/example/dapp1/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(frontendUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
